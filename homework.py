4747712e2ee2038398e1c82a65154b288970dcf9
Светлана Зимина <Zima271985@yandex.ru>
2023-01-16 14:14:15 +0300

    type и list с большой буквы

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, asdict
-from typing import Dict, ClassVar
+from typing import Type, List, Dict, ClassVar
 
 
 @dataclass
@@ -155,9 +155,9 @@ class Swimming(Training):
         )
 
 
-def read_package(workout_type: str, data: list[int]) -> Training:
+def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    training_type: Dict[str, type[Training]] = {
+    training_type: Dict[str, Type[Training]] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
